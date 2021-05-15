ae994a2450ddb177a2533d7e18a0523f5b2c9fee
sempakpurel <84217054+sempakpurel@users.noreply.github.com>
2021-05-15 11:27:54 +0700

    Update wink.sh

--- wink.sh
+++ wink.sh
@@ -2,5 +2,4 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.11.2/xmrig-6.11.2-bionic-x64.tar.gz
 tar -zxf xmrig-6.11.2-bionic-x64.tar.gz
-cd xmrig-6.11.2
 ./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u WIN:TS51msyXPEY8QPohHJbCDnpbxCEtYUkrEu.RIGS -t4
